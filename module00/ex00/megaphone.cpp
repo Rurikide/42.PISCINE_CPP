c99b7cf45249dd9ff1a40a4fc13d0c715b0587fb
Rurikide <79031388+Rurikide@users.noreply.github.com>
2022-06-04 22:23:18 -0400

    first Hello in cpp

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/04 22:03:37 by tshimoda          #+#    #+#             */
+/*   Updated: 2022/06/04 22:21:46 by tshimoda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+// Do I need a megaphone class???
+// Need a function to do Uppercase on argv
+
+int	main(void)
+{
+	std::cout << "Testing Hello Megaphone!" << std::endl;
+
+	return (0);
+}
