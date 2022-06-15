1cd7e02adc1d57a3fb282082bac515aa2397b236
Tokutaro Shimoda <tshimoda@c1r7p5.42quebec.com>
2022-06-15 12:31:08 -0400

    define color

--- module01/ex00/Define.hpp
+++ module01/ex00/Define.hpp
@@ -6,7 +6,7 @@
 /*   By: tshimoda <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 14:49:59 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/06/12 14:54:55 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/06/15 12:28:13 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 #define DEFINE_HPP
 
 #define AKAI "\e[0;31m"
+#define KIIRO "\e[0;33m"
 #define MIDORI "\e[0;32m"
 #define SORAIRO "\e[0;36m"
-#define MURASAKI "\e[35m"
+#define MURASAKI "\e[0;35m"
 #define END_COLOR "\e[0m"
 
 #define SUCCESS 0
--- module01/ex00/Zombie.cpp
+++ module01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: tshimoda <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 14:20:45 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/06/14 15:49:07 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/06/15 12:29:21 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 #include "Zombie.hpp"
 #include "Define.hpp"
 
-Zombie::Zombie( void ) {}
+Zombie::Zombie( void )
+{
+	std::cout << KIIRO << "A wild Zombie appeared!!!" END_COLOR << std::endl;
+}
 
 Zombie::~Zombie( void )
 {
-	std::cout << SORAIRO << this->getName() << " is destroyed..." END_COLOR << std::endl;
+	std::cout << SORAIRO << this->getName() << " has been vanquished..." END_COLOR << std::endl;
 }
 
 std::string	Zombie::getName( void )
