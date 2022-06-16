86959f6c3e5188ca934ef488c275495159d6f4c3
Tokutaro Shimoda <tshimoda@c1r7p5.42quebec.com>
2022-06-16 14:57:54 -0400

    start HumanB

--- module01/ex03/HumanB.hpp
+++ module01/ex03/HumanB.hpp
@@ -1,34 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   HumanA.hpp                                         :+:      :+:    :+:   */
+/*   HumanB.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tshimoda <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/16 11:49:02 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/06/16 14:51:17 by tshimoda         ###   ########.fr       */
+/*   Created: 2022/06/16 14:55:17 by tshimoda          #+#    #+#             */
+/*   Updated: 2022/06/16 14:57:32 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HUMANA_HPP
-#define HUMANA_HPP
+#ifndef HUMANB_HPP
+#define HUMANB_HPP
 
 #include "Weapon.hpp"
 
-// HumanA takes a Weapon in its constructor and is always armed. &references are always initialized, unlike pointers.
+// HumanB's weapon is not initialized in their constructor and is not always armed.
 
-class HumanA {
+class HumanB {
 
 	private:
 		std::string _name;
-		Weapon&		_weapon;
+		Weapon*		_weapon;
 	
 	public:
-		HumanA( std::string name, Weapon& weapon );
-		~HumanA( void );
+		HumanB( std::string name );
+		~HumanB( void );
 		
 		std::string	getName( void ) const;
-		Weapon&		getWeapon( void ) const;
+		Weapon*		getWeapon( void ) const;
+		void		setWeapon( std::string );
 		void		attack( void );
 };
 
