a454f37ad149e9f846b13b082523f44b317959b9
Tokutaro Shimoda <tshimoda@c1r7p5.42quebec.com>
2022-06-16 11:08:28 -0400

    in-built .empty() in string

--- module01/ex01/zombieHorde.cpp
+++ module01/ex01/zombieHorde.cpp
@@ -6,15 +6,19 @@
 /*   By: tshimoda <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 14:20:04 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/06/15 15:32:45 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/06/16 10:56:10 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 #include "Zombie.hpp"
 
 Zombie*	zombieHorde( int N, std::string name )
 {
+	if ( N < 1 || name.empty() == true)
+		return NULL;
+
 	Zombie*	horde = new Zombie[N];
 	
 	for (int i = 0; i < N; i++)
--- module01/ex02/main.cpp
+++ module01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tshimoda <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 15:12:50 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/06/15 15:37:06 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/06/16 10:42:32 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	main( void )
 	std::cout << MIDORI "memory address of strRef: " << &strRef << END_COLOR << std::endl;
 
 	std::cout << KIIRO "value of message: " << message << END_COLOR << std::endl;
-	std::cout << SORAIRO "value pointed to by strPtr: " << &strPtr << END_COLOR <<  std::endl;
+	std::cout << SORAIRO "value pointed to by strPtr: " << *strPtr << END_COLOR <<  std::endl;
 	std::cout << MIDORI "value pointed to by strRef: " << strRef << END_COLOR << std::endl;
 
 	return 0;
