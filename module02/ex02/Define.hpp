9fb32c438b3266d96d3604c67548277e291ff227
Tokutaro Shimoda <tshimoda@c1r7p5.42quebec.com>
2022-06-19 09:42:35 -0400

    added AOI

--- module02/ex02/Define.hpp
+++ module02/ex02/Define.hpp
@@ -6,13 +6,14 @@
 /*   By: tshimoda <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 14:49:59 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/06/15 12:28:13 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/06/19 09:40:51 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFINE_HPP
 #define DEFINE_HPP
 
+#define AOI	"\e[0;34m"
 #define AKAI "\e[0;31m"
 #define KIIRO "\e[0;33m"
 #define MIDORI "\e[0;32m"
