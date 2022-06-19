365046c671076de59954de4bdde200d8a3ac15a6
Tokutaro Shimoda <tshimoda@c1r7p5.42quebec.com>
2022-06-19 10:06:25 -0400

    canonical

---
+++ module02/ex00/Fixed.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tshimoda <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/19 09:43:32 by tshimoda          #+#    #+#             */
+/*   Updated: 2022/06/19 10:05:50 by tshimoda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+
+	private: 
+		static const int	_fractionalBits; // important to initialize it in the .cpp
+		int					_value;
+	
+	public: //Orthodox Canonical Form
+		Fixed( void ); // default constructor
+		Fixed( Fixed& const src ); // copy constructor
+		~Fixed( void ); // destructor, no virtual yet in module02
+		Fixed& operator=( Fixed& const rhs);
+
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );	
+};
+
+#endif
