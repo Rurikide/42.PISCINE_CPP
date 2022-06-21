ff5b5713c09efc41e8be71eb5f87ddf572146b15
Tokutaro Shimoda <tshimoda@c2r3p7.42quebec.com>
2022-06-21 16:26:38 -0400

    setting up module04

---
+++ module04/ex00/Animal.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tshimoda <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 16:14:38 by tshimoda          #+#    #+#             */
+/*   Updated: 2022/06/21 16:17:16 by tshimoda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <string> // std::string
+
+class Animal {
+
+	protected:
+		std::string	type;
+};
+
+#endif
