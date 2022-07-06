44eb8a94cc30e67a2ee5aadb612c1747b2a0d65f
Tokutaro Shimoda <tshimoda@c1r7p5.42quebec.com>
2022-07-06 10:35:58 -0400

    Piscine C++ done

--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tshimoda <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 10:34:56 by tshimoda          #+#    #+#             */
+/*   Updated: 2022/07/06 10:35:00 by tshimoda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "MutantStack.hpp"
 #include "define.hpp"
 #include <iostream>
@@ -19,6 +31,25 @@ int	main(void)
 			std::cout << SORAIRO << *it << END_COLOR << std::endl;
 
 		std::cout << KIIRO << "rubberduck's mutant string size == " << mString.size() << END_COLOR << std::endl;
+	}
+	{
+		MutantStack<double> mDouble;
+		mDouble.push(1.0);
+		mDouble.push(-2.4);
+		mDouble.push(8.9);
+		mDouble.push(1000.18);
+		mDouble.push(-777.18);
+		mDouble.push(13.444);
+		
+		mDouble.pop();
+
+		MutantStack<double>::iterator it = mDouble.begin();
+		MutantStack<double>::iterator ite = mDouble.end();
+
+		for (; it != ite; it++)
+			std::cout << SORAIRO << *it << END_COLOR << std::endl;
+
+		std::cout << KIIRO << "rubberduck's mutant string size == " << mDouble.size() << END_COLOR << std::endl;
 	}
 
 	/*----------SUBJECT-TEST-------------*/
