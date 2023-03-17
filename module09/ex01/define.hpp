3471eda0437c1f32298a86e72e18f99512688af0
Rurikide <quebec.japonais@gmail.com>
2023-03-17 09:24:51 -0400

    setting up module09 folders

---
+++ module09/ex01/define.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   define.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tshimoda <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/12 14:49:59 by tshimoda          #+#    #+#             */
+/*   Updated: 2022/06/23 10:54:52 by tshimoda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINE_HPP
+#define DEFINE_HPP
+
+#define AOI "\e[0;34m"
+#define AKAI "\e[0;31m"
+#define KIIRO "\e[0;33m"
+#define AIZOME "\e[0;49;34m"
+#define MIDORI  "\e[0;32m"
+#define SORAIRO  "\e[0;36m"
+#define MOMOIRO   "\e[0;35m"
+#define GRAYGREY   "\e[1;30m"
+#define MURASAKI    "\e[1;34m"
+#define SHIROIOOI    "\e[1;39m"
+#define END_COLOR     "\e[0m"
+
+
+#define SUCCESS 0
+#define FAIL -1
+
+#endif
